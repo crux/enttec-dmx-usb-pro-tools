3957fb1be2d386199c9d17efc66a8ebbbf750695
dirk luesebrink <dirk@sebrink.de>
2009-11-06 21:32:53 +0100

    switched to gom-remote gem
    and removed include gom classes and tests from here.

--- lib/gom/remote.rb
+++
@@ -1,5 +0,0 @@
-require 'gom/remote/connection'
-require 'gom/remote/entry'
-require 'gom/remote/subscription'
-require 'gom/remote/callback_server'
-require 'gom/remote/daemon'
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,5 +1,6 @@
 $LOAD_PATH.unshift(File.dirname(__FILE__))
 $LOAD_PATH.unshift(File.join(File.dirname(__FILE__), '..', 'lib'))
+#$LOAD_PATH.unshift File.join(File.dirname(__FILE__), %w{.. .. gom-remote lib})
 require 'enttec-dmx-usb-pro-tools'
 require 'spec'
 require 'spec/autorun'
